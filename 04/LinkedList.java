4e094dbfbaa4d4e4fc09ae1341c6396d737cbda8
Dimitri Kurashvili <dimakura@gmail.com>
2020-07-09 23:05:42 +0400

    Exercise 4.1

---
+++ 04/LinkedList.java
@@ -0,0 +1,51 @@
+public class LinkedList<K extends Comparable<K>> {
+  private K key;
+  private LinkedList<K> next;
+
+  LinkedList(K key) {
+    this.key = key;
+  }
+
+  LinkedList<K> append(K key) {
+    var list = this;
+    while (list.next != null)
+      list = list.next;
+    return list.next = new LinkedList<>(key);
+  }
+
+  void print() {
+    var element = this;
+    while (element != null) {
+      System.out.print(element.key);
+      System.out.print(" ");
+      element = element.next;
+    }
+    System.out.println();
+  }
+
+  // Exercise 4.1
+  LinkedList<K> delete(K key) {
+    if (key.compareTo(this.key) == 0) {
+      return next;
+    } else {
+      var parent = this;
+      var element = parent.next;
+      while (element != null) {
+        if (element.key.compareTo(key) == 0) {
+          parent.next = element.next;
+          break;
+        }
+        parent = element;
+        element = parent.next;
+      }
+
+      return this;
+    }
+  }
+
+  // Exercise 4.2
+  LinkedList<K> reverse() {
+    // TODO:
+    return this;
+  }
+}
---
+++ 04/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    // 4.1
+    var list = new LinkedList<Integer>(1);
+    list.append(2).append(3).append(4).append(5);
+    list.print();
+    list.delete(1).print();
+  }
+}
